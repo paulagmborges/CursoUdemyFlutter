214d0d4b3a75ad6bc68c82c0e690e54e4985eaa6
Paula Borges <100873574+paulagmborges@users.noreply.github.com>
2022-09-04 18:45:55 -0300

    Add files via upload

---
+++ cursodart/01Fundamentos/constante_1.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+main() {
+  //Área da circunferência = PI * raio *raio
+
+  var entradaDoUsuario = stdin.readLineSync();
+   var raio = double.parse(entradaDoUsuario);
+  print("O valor do raio é:");
+}
---
+++ cursodart/01Fundamentos/primeiro.dart
@@ -0,0 +1,12 @@
+
+main() 
+{
+  print('olá Dart');
+
+ {
+  print('Fim!');
+ }
+}
+
+//Porta de entrada de um prograna dart é o main.
+//Parenteses são muito importantes,pode colocar ASPAS '' SIMPES OU DULPAS
