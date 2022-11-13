fdc2a67853cde35c60995e42db96cccdc684a782
Paula Borges <100873574+paulagmborges@users.noreply.github.com>
2022-11-13 14:06:18 -0300

    Delete cursodart/01Fundamentos directory

--- cursodart/01Fundamentos/constante_1.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-main() {
-  //Área da circunferência = PI * raio *raio
-
-  var entradaDoUsuario = stdin.readLineSync();
-   var raio = double.parse(entradaDoUsuario);
-  print("O valor do raio é:");
-}
--- cursodart/01Fundamentos/variaveis_1.dart
+++
@@ -1,7 +0,0 @@
-main() {
-  int a = 2; //sentença de codigo
-  double b = 3.1314;
-  b = 3.1415;
-  print(a);
-  print(1 + 2 * 4);
-}
